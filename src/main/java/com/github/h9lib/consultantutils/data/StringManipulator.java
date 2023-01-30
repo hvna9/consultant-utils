7e22dba0b8cd16e1d5d94ce8b11805605e57e7da
claudiodimauro <claudio.dimauro@nttdata.com>
2023-01-30 18:08:47 +0100

    add string utility

---
+++ src/main/java/com/github/h9lib/consultantutils/data/StringManipulator.java
@@ -0,0 +1,80 @@
+package com.github.h9lib.consultantutils.data;
+
+import java.nio.charset.Charset;
+import java.util.Random;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.github.h9lib.consultantutils.constants.Placeholder;
+
+public class StringManipulator {
+	
+	/**
+	 * This method is defined to reverse the string passed as parameter.
+	 * 
+	 * @param str
+	 * @return the reversed string
+	 */
+	public static String reverse(String str) {
+		if(StringUtils.isEmpty(str))
+			throw new IllegalArgumentException("Invalid string");
+		
+		if(str.length() < 2)
+			return str;
+		
+		char[] normal = str.toCharArray();
+		char[] reversed = new char[normal.length];
+		
+		int strLen = normal.length;
+		
+		for(int i = 0; i < strLen; i++) {
+			reversed[i] = normal[strLen - (i+1)];
+		}
+		
+		return new String(reversed);
+	}
+	
+	/**
+	 * This method return a placeholder string with a specified length.
+	 * The specified length correspond to the number of characters in the returned string.
+	 * 
+	 * It returns a "Lorem Ipsum" text, if you use a fixed size of 10, 100, 200, 500 or 1000.
+	 * It returns a random string with a number of character specified by size parameter.
+	 * 
+	 * @param size
+	 * @return a placeholder string
+	 */
+	public static String generatePleaceholder(int size) {
+		if(size == 10)
+			return Placeholder.LOREM_IPSUM_10;
+		if(size == 100)
+			return Placeholder.LOREM_IPSUM_100;
+		if(size == 200)
+			return Placeholder.LOREM_IPSUM_200;
+		if(size == 500)
+			return Placeholder.LOREM_IPSUM_500;
+		if(size == 1000)
+			return Placeholder.LOREM_IPSUM_1000;
+		
+		if(size < 1000)
+			return Placeholder.LOREM_IPSUM_1000.substring(0, size);
+		else {
+			int multiplier = (int) Math.ceil(size/1000d);
+			
+			String concatString = "";
+			for( ; multiplier > 0; multiplier--) {
+				concatString += Placeholder.LOREM_IPSUM_1000 + " ";
+			}
+			
+			String retVal = concatString.substring(0, size);
+			
+			//Terminate all phrases with '.'
+			if(retVal.endsWith(", ")) 
+				return retVal.substring(0, retVal.length() - 2) + ".";
+			else 
+				return retVal.substring(0, retVal.length() - 1) + ".";
+		}
+	}
+
+}
